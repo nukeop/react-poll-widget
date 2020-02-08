125871bfd96c648eeb853d24a5df8a920e14aa7b
nukeop <nukeop@gumblert.tech>
2020-02-08 14:07:38 +0100

    update

--- packages/ui/src/components/Icon/Icon.tsx
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
-export const Icon = () => <box-icon />;
+export const Icon: React.FC<{
+    type: 'regular' | 'solid' | 'logo',
+    name: string,
+    color: string,
+    size: 'xs' | 'sm' | 'md' | 'lg' | 'cssSize',
+    rotate: '90' | '180' | '270',
+    flip: 'horizontal' | 'vertical',
+    border: 'square' | 'circle',
+    animation: string,
+    pull: 'left' | 'right'
+}> = (props) => <box-icon {...props} />;
