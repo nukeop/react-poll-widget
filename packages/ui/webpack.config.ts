e3743a989c5aca2cb7170a6bb95753ac469594f6
nukeop <nukeop@gumblert.tech>
2020-03-04 01:28:25 +0100

    Webpack bundling

---
+++ packages/ui/webpack.config.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+    {
+       test: /\.(js|jsx|tsx|ts)$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ttf|eot|woff|woff2|svg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
+
+export default config;
