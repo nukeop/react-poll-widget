20deefb3556fb4b6c70b2a0242321a26088064aa
nukeop <nukeop@gumblert.tech>
2020-06-02 00:22:52 +0200

    Transition to hooks

--- packages/widget/src/components/PollWidget/index.tsx
+++ packages/widget/src/components/PollWidget/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import _ from 'lodash';
-import { compose, withHandlers, withState } from 'recompose';
 import {
   Message,
   TransitionablePortal,
@@ -43,20 +42,24 @@ const PollComponent: React.FC<{ currentStep: PollStep }> = ({ currentStep }) => 
   }
 }
 
-type PollWidgetProps = {
+type PollWidgetContainerProps = {
   trigger: React.ReactNode;
   transition?: TransitionProps;
   color: PanelColor;
   steps: PollStep[];
-  step: number;
-  setStep: (i: number) => void;
+
   onOpen: (event: null, data: PortalProps & TransitionablePortalState) => void;
   onClose: (event: null, data: PortalProps & TransitionablePortalState) => void;
+}
+
+type PollWidgetComponentProps = PollWidgetContainerProps & {
+  step: number;
+  setStep: (i: number) => void;
   goBack: () => void;
   goForward: () => void;
 };
 
-const PollWidget: React.FC<PollWidgetProps> = ({
+const PollWidgetComponent: React.FC<PollWidgetComponentProps> = ({
   trigger,
   transition,
   color,
@@ -68,44 +71,62 @@ const PollWidget: React.FC<PollWidgetProps> = ({
   goBack,
   goForward
 }) => {
-    const currentStep: PollStep = steps[step];
-    return (
-      <TransitionablePortal
-        closeOnTriggerClick
-        closeOnDocumentClick={false}
-        trigger={trigger}
-        transition={transition}
-        onOpen={onOpen}
-        onClose={onClose}
+  const currentStep: PollStep = steps[step];
+  return (
+    <TransitionablePortal
+      closeOnTriggerClick
+      closeOnDocumentClick={false}
+      trigger={trigger}
+      transition={transition}
+      onOpen={onOpen}
+      onClose={onClose}
+    >
+      <Panel
+        color={color}
+        hasHeader={!_.isNull(currentStep?.description)}
+        hasFooter
+        hasSteps={steps.length > 1}
+        headerContent={currentStep?.title}
       >
-        <Panel
-          color={color}
-          hasHeader={!_.isNull(currentStep?.description)}
-          hasFooter
-          hasSteps={steps.length > 1}
-          headerContent={currentStep?.title}
-        >
-          <PanelContentHeader>
-            {currentStep?.header}
-          </PanelContentHeader>
-          {currentStep?.description}
-          <PollComponent currentStep={currentStep} />
-          <PanelContentFooter
-            hasNext={step < steps.length - 1}
-            hasBack={step > 0}
-            hasSubmit={step === steps.length - 1}
-            onBack={goBack}
-            onNext={goForward}
-          />
-        </Panel>
-      </TransitionablePortal>
-    )
-  }
+        <PanelContentHeader>
+          {currentStep?.header}
+        </PanelContentHeader>
+        {currentStep?.description}
+        <PollComponent currentStep={currentStep} />
+        <PanelContentFooter
+          hasNext={step < steps.length - 1}
+          hasBack={step > 0}
+          hasSubmit={step === steps.length - 1}
+          onBack={goBack}
+          onNext={goForward}
+        />
+      </Panel>
+    </TransitionablePortal>
+  )
+}
 
-export default compose<PollWidgetProps, Omit<PollWidgetProps, 'step' | 'setStep' | 'goBack' | 'goForward'>>(
-  withState('step', 'setStep', 0),
-  withHandlers({
-    goBack: ({ setStep, step }: PollWidgetProps) => () => setStep(Math.max(step - 1, 0)),
-    goForward: ({ setStep, step, steps }: PollWidgetProps) => () => setStep(Math.min(step + 1, steps.length - 1))
-  })
-)(PollWidget);
+const PollWidget: React.FC<PollWidgetContainerProps> = ({
+  steps,
+  ...rest
+}) => {
+  const [step, setStep] = useState(0);
+  const goBack = useCallback(
+    () => setStep(Math.max(step - 1, 0)),
+    [step, setStep]
+  );
+  const goForward = useCallback(
+    () => setStep(Math.min(step + 1, steps.length - 1)),
+    [steps, step, setStep]
+  );
+
+  return <PollWidgetComponent
+    steps={steps}
+    step={step}
+    setStep={setStep}
+    goBack={goBack}
+    goForward={goForward}
+    {...rest}
+  />
+}
+
+export default PollWidget;
